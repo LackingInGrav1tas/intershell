2257ee27bc64b7311659d602c1ab37dc815378a6
LackingInGrav1tas <42680395+LackingInGrav1tas@users.noreply.github.com>
2021-06-28 14:06:04 -0400

    dir

--- src/cmd.rs
+++ src/cmd.rs
@@ -3,6 +3,13 @@ use std::io;
 use std::env;
 use std::path::Path;
 use std::str;
+use std::fs;
+
+macro_rules! needs_rendering {
+    () => {
+        println!("needs re-rendering")
+    };
+}
 
 use crate::parser::{parse, TOMLCommand, CommandType};
 
@@ -40,7 +47,9 @@ impl Shell {
                 self.run_builtin_command(s)
             }
             CommandType::CMDCall(s) => {
-                // override everything and send straight to normal cmd
+                let mut c = s.chars();
+                c.next();
+                self.run_builtin_command(String::from(c.as_str()))
             }
         }
     }
@@ -53,13 +62,27 @@ impl Shell {
             cmd.arg(a);
         }
         println!("running {:?}", cmd);
-        cmd.current_dir(&self.cwd);
+        cmd.current_dir(&self.get_cwd());
         cmd.status()
+    }
+
+    fn run_cmd_command(&mut self, args: &mut Vec<&str>) {
+        // runs a cmd command
+        let mut cmd = Command::new(args[0]);
+        args.remove(0);
+        self.history.push(format!("{:?}", cmd));
+        for a in args {
+            cmd.arg(a);
+        }
+        cmd.current_dir(&self.get_cwd());
+        println!("running {:?}", cmd);
+        cmd.status().unwrap();
     }
 
     fn run_builtin_command(& mut self, command: String) {
         // runs a built in command
-        let args = command.split(" ").collect::<Vec<&str>>();
+        let mut args = command.split(" ").collect::<Vec<&str>>();
+        // args.remove(0);
         match args.get(0).expect("no command given.").clone() {
             "cd" => {
                 let newdir: String;
@@ -79,19 +102,33 @@ impl Shell {
                 )
             }
             "dir" => {
-
+                println!("CONTENTS OF {}", self.get_cwd());
+                let paths = fs::read_dir(self.get_cwd()).unwrap();
+                for path in paths {
+                    let file_path = path.unwrap().path();
+                    let metadata = match fs::metadata(file_path.clone()) {
+                        Ok(data) => data.len(),
+                        Err(_) => 0,
+                    };
+                    println!("{} - {} bytes", file_path.display().to_string().replace(&self.get_cwd(), "."), metadata)
+                }
+                println!();
             }
             "md" | "mkdir" => {
-
+                fs::create_dir_all(self.get_cwd() + args.get(1).unwrap()).unwrap();
             }
             "del" | "delete" | "erase" => {
-
-            }
-            "attrib" | "attributes" => {
-
+                match fs::remove_file(self.get_cwd() + args.get(1).unwrap()) {
+                    Ok(_) => (),
+                    Err(_) => {
+                        fs::remove_dir(self.get_cwd() + args.get(1).unwrap()).unwrap()
+                    }
+                }
             }
             "cls" | "clear" => {
-
+                // std::process::Command::new("cls").status().unwrap();
+                print!("{}[2J", 27 as char);
+                needs_rendering!();
             }
             "exit" | "quit" => {
                 std::process::exit(0)
@@ -100,31 +137,38 @@ impl Shell {
 
             }
             "comp" | "compare" => {
-
+                args[0] = "comp";
+                self.run_cmd_command(& mut args)
             }
             "copy" => {
-
+                self.run_cmd_command(& mut args)
             }
             "echo" => {
-
+                let mut c = std::process::Command::new("cmd");
+                c.arg("/c");
+                c.arg("echo");
+                c.arg(args.get(1).expect("expected an additional arg")); 
+                println!("{:?}", c);
+                c.status().unwrap();
             }
             "find" => {
-
+                self.run_cmd_command(& mut args)
             }
             "print" => {
-
+                self.run_cmd_command(& mut args)
             }
             "rename" => {
-
+                self.run_cmd_command(& mut args)
             }
             "rmdir" => {
-
+                self.run_cmd_command(& mut args)
             }
             "ip" | "ipconfig" => {
-                
+                args[0] = "ipconfig";
+                self.run_cmd_command(& mut args)
             }
-            _ => {
-                println!("should not be reachable, value {:?}", command.to_lowercase().as_str());
+            com => {
+                println!("should not be reachable, value {:?}", com);
                 std::process::exit(0)
             }
         }
